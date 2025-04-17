a87d956e728098b597dfff16a02941bd21c11cbf
Денис Бояринов <boyarinov.denis@icloud.com>
2025-04-17 15:39:10 +0300

    widget module have implemented.

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+import datetime
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(score_info: str) -> str:
+    """Функция принимает на вход строку формата 'Счет <score_number>' или '<Card> <card_number>' и возвращает маскированную строку"""
+    score_info_split: list[str] = score_info.split()
+    masked_info: str = ""
+
+    if score_info_split[0] == "Счет":
+        masked_info = get_mask_account(score_info.split()[-1])
+    else:
+        masked_info = get_mask_card_number(score_info.split()[-1])
+
+    return f"{' '.join(score_info_split[:-1])} {masked_info}"
+
+
+def get_date(date_string: str) -> str:
+    """Функция принимает на вход строку с датой в формате iso 8601 и возвращает строку в формате '%d.%m.%Y'"""
+    date_decoded = datetime.datetime.fromisoformat(date_string)
+
+    return date_decoded.strftime("%d.%m.%Y")
