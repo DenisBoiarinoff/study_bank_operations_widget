7c71715bd0fa63839e503fbfe53483e63c2c9c18
Денис Бояринов <boyarinov.denis@icloud.com>
2025-04-29 15:06:00 +0300

    Data validation

--- src/masks.py
+++ src/masks.py
@@ -1,8 +1,14 @@
 def get_mask_card_number(card_number: str) -> str:
     """ "Функция принимает на вход номер карты и возвращает ее маску."""
-    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    trimmed_card_number = card_number.replace(" ", "")
+    if not trimmed_card_number.isdigit() or len(trimmed_card_number) != 16:
+        raise ValueError("Invalid card number")
+    return f"{trimmed_card_number[:4]} {trimmed_card_number[4:6]}** **** {trimmed_card_number[-4:]}"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
-    return f"**{account_number[-4:]}"
+    trimmed_account = account_number.replace(" ", "")
+    if not trimmed_account.isdigit() or len(trimmed_account) < 6:
+        raise ValueError("Invalid account number")
+    return f"**{trimmed_account[-4:]}"
