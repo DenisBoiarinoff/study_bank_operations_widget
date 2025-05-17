aa11c3a6dc974d1018640a55574b737041d3c1fd
Денис Бояринов <boyarinov.denis@icloud.com>
2025-05-17 14:46:47 +0300

    Decorators module have impplemented, decorators tests have implemented.

---
+++ src/decorators.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from typing import Optional, Callable
+
+
+def write_result(message: str, filename: Optional[str] = None) -> None:
+    """Записывает переданное сообщение в файл, если предано имя, или в консоль, в противном случае."""
+    if filename is None:
+        print(message)
+    else:
+        with open(filename, "w") as file:
+            file.write(message)
+
+
+def log(filename: Optional[str] = None):
+    """Декоратор для логирования функций и вывода результатов в консоль или файл"""
+
+    def decorator(func):
+        @wraps(func)
+        def inner(*args, **kwargs):  # type: ignore
+            try:
+                result = func(*args, **kwargs)
+                write_result(f"\n{func.__name__} ok\n", filename=filename)
+            except Exception as e:
+                write_result(
+                    f"\n{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}\n", filename=filename
+                )
+                raise e
+
+            return result
+
+        return inner
+
+    return decorator
---
+++ tests/test_decorators.py
@@ -0,0 +1,102 @@
+import os
+
+import pytest
+
+from src.decorators import log
+
+
+@log()
+def console_decorated_console_function(x, y):
+    return x + y
+
+
+@log("tmp.txt")
+def console_decorated_file_function(x, y):
+    return x + y
+
+
+@pytest.mark.parametrize(
+    "first, second",
+    [
+        (1, [1]),
+        (1, (1,)),
+        (1, "1"),
+    ],
+)
+def test_decorator_error(first, second):
+    with pytest.raises(TypeError):
+        console_decorated_console_function(first, second)
+
+
+@pytest.mark.parametrize(
+    "first, second, console_output",
+    [
+        (1, 1, "\nconsole_decorated_console_function ok\n\n"),
+        ("1", "1", "\nconsole_decorated_console_function ok\n\n"),
+        (True, True, "\nconsole_decorated_console_function ok\n\n"),
+    ],
+)
+def test_decorator_success_console_output(capsys, first, second, console_output):
+    console_decorated_console_function(first, second)
+    captured = capsys.readouterr()
+    assert captured.out == console_output
+
+
+@pytest.mark.parametrize(
+    "first, second, console_output",
+    [
+        (1, [1], "\nconsole_decorated_console_function error: TypeError. Inputs: (1, [1]), {}\n\n"),
+        (1, (1,), "\nconsole_decorated_console_function error: TypeError. Inputs: (1, (1,)), {}\n\n"),
+        (1, "1", "\nconsole_decorated_console_function error: TypeError. Inputs: (1, '1'), {}\n\n"),
+    ],
+)
+def test_decorator_error_console_output(capsys, first, second, console_output):
+    try:
+        console_decorated_console_function(first, second)
+    except Exception:
+        captured = capsys.readouterr()
+        assert captured.out == console_output
+
+
+@pytest.mark.parametrize(
+    "first, second, file_string",
+    [
+        (1, [1], "console_decorated_file_function error: TypeError. Inputs: (1, [1]), {}\n"),
+        (1, (1,), "console_decorated_file_function error: TypeError. Inputs: (1, (1,)), {}\n"),
+        (1, "1", "console_decorated_file_function error: TypeError. Inputs: (1, '1'), {}\n"),
+    ],
+)
+def test_decorator_error_file_output(first, second, file_string):
+    try:
+        console_decorated_file_function(first, second)
+    except Exception:
+        with open("tmp.txt", "r") as file:
+            lines = file.readlines()
+            assert len(lines) == 2
+            assert lines[1] == file_string
+    else:
+        assert False
+    finally:
+        os.remove("tmp.txt")
+
+
+@pytest.mark.parametrize(
+    "first, second, file_string",
+    [
+        (1, 1, "console_decorated_file_function ok\n"),
+        ("1", "1", "console_decorated_file_function ok\n"),
+        (True, True, "console_decorated_file_function ok\n"),
+    ],
+)
+def test_decorator_success_file_output(first, second, file_string):
+    try:
+        console_decorated_file_function(first, second)
+    except Exception:
+        assert False
+    else:
+        with open("tmp.txt", "r") as file:
+            lines = file.readlines()
+            assert len(lines) == 2
+            assert lines[1] == file_string
+    finally:
+        os.remove("tmp.txt")
